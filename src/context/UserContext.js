502baccd75be66a6c3938bffce9b1d387ef8340a
Dhanya A R <dhanyaar29@gmail.com>
2025-08-07 23:09:12 +0530

    Create UserContext.js

---
+++ src/context/UserContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [username, setUsername] = useState(() => localStorage.getItem("username") || null);
+
+  useEffect(() => {
+    if (username) {
+      localStorage.setItem("username", username);
+    } else {
+      localStorage.removeItem("username");
+    }
+  }, [username]);
+
+  return (
+    <UserContext.Provider value={{ username, setUsername }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
