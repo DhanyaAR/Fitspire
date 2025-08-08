fa96ac600646613bb5032b6c26cfc5e9f758a770
Dhanya A R <dhanyaar29@gmail.com>
2025-08-08 14:09:42 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: "#39FF14",
+        darkGrey: "#1a1a1a"
+      }
+    }
+  },
+  darkMode: "class",
+  plugins: [],
+};
